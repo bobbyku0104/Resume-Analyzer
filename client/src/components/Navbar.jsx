42da68c666967e44b2065e4eb72b446600c7f1b7
bobbyku0104 <143bobby123@gmail.com>
2025-12-19 17:41:14 +0530

    Navbar color Changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="w-[95%] max-w-[1150px] mx-auto mt-4 bg-white shadow-md py-2 px-5 flex items-center justify-between rounded-full">
+    <nav className="w-[95%] max-w-[1150px] mx-auto mt-4 bg-indigo-500 shadow-md py-2 px-5 flex items-center justify-between rounded-full">
 
-      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-black">
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-white">
         Resume
       </h1>
 
-      <button className="bg-indigo-600 text-white text-sm sm:text-base px-4 py-2 rounded-full hover:bg-indigo-700 transition">
+      <button className="bg-indigo-300 text-black text-sm sm:text-base px-4 py-2 rounded-full hover:bg-indigo-700 transition">
         Upload Resume
       </button>
 
